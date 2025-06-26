89b529c6554a4a428afaa9aba5fc0294e7bf3e50
Lucien Clin <lucien.clin@uni-tuebingen.de>
2025-06-26 14:42:10 +0200

    feat: Added init.sh; Simplified forward-proxy.template.conf

---
+++ init.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+# Create non-template copies of all template files, unless already present
+
+for templateFile in config/*.template.*; do
+
+  file=${templateFile/.template}	
+
+  if [ ! -f file ]; then cp ${templateFile} ${file}; fi
+done	
+
+if [ ! -d nginx/sites-enabled ]; then mkdir nginx/sites-enabled; fi
+
+for templateFile in nginx/sites-available/*.template.*; do
+
+   name=${templateFile##*/}
+
+   file=nginx/sites-enabled/${name/.template}
+
+  if [ ! -f file ]; then cp ${templateFile} ${file}; fi
+done
+
+
+if [ ! -f .env ]; then cp .env.template .env; fi
+
